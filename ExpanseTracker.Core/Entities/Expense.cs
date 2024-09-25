64fc86fc262b7b65fce086613e15eb87da84c8eb
EduardoKroetz <eduardokroetz0@gmail.com>
2024-09-25 17:47:25 -0300

    feat: separate the project into layers, shape the entities, create database context and configure connection to the postgreSQL database

---
+++ ExpanseTracker.Core/Entities/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpanseTracker.Core.Entities;
+
+public class Expense
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ ExpanseTracker.Infra/Persistence/ExpanseTrackerDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ExpanseTracker.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpanseTracker.Infra.Persistence;
+
+public class ExpanseTrackerDbContext : DbContext
+{
+    public ExpanseTrackerDbContext(DbContextOptions<ExpanseTrackerDbContext> optionsBuilder) : base(optionsBuilder)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>();
+
+        builder.Entity<Expense>(x =>
+        {
+            x.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(e => e.UserId);
+            x.HasOne(x => x.Category)
+                .WithMany()
+                .HasForeignKey(e => e.CategoryId);
+        });
+
+        builder.Entity<Category>();
+    }
+}
